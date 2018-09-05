d081f9f754f582f450ad58b3c6cb16520dd07806
julycabigas <july@businessblueprint.com>
2018-09-05 09:10:09 +0800

    update top video

--- template-parts/modules/top_video.php
+++ template-parts/modules/top_video.php
@@ -3,13 +3,13 @@
 
 	<div class="container">
 
-		<?php if( get_sub_field('heading') ) : ?>
+		<?php if( get_sub_field('video_heading') ) : ?>
 			<div class="module__header module__hero-header">
 
-	 	  	  	<?php if( get_sub_field('subheading') )  { ?>
-	 	  	  			<h2 class="subtitle"><?php echo get_sub_field('subheading') ?></h2> 
+	 	  	  	<?php if( get_sub_field('video_subheading') )  { ?>
+	 	  	  			<h2 class="subtitle"><?php echo get_sub_field('video_subheading') ?></h2> 
 	 	  	  	<?php } ?>	
-	 	  	  	<h1 class="title"><?php echo get_sub_field('heading') ?></h1>
+	 	  	  	<h1 class="title"><?php echo get_sub_field('video_heading') ?></h1>
 
 
 	 	  	</div><!-- ./hero__header -->
@@ -20,8 +20,8 @@
 		 	<div class="col-sm-12">
 
 		 	    <div class="module__hero-video">
-		 	    	<?php if( get_sub_field('video') )  { ?>
-		 	 			<?php echo get_sub_field('video') ?>
+		 	    	<?php if( get_sub_field('top_video') )  { ?>
+		 	 			<?php echo get_sub_field('top_video') ?>
 		 	 		<?php } ?>
 		 	 	</div><!-- ./video -->
 
